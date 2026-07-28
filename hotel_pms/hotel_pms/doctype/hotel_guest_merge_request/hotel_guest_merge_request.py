df0973a89fc3b8c3303a81dc406db7fa25bedff7
augustinussent <augustinussent@gmail.com>
2026-07-28 08:32:56 +0700

    Add missing doctype python controllers

---
+++ hotel_pms/hotel_pms/doctype/hotel_guest_merge_request/hotel_guest_merge_request.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, The Batu Hotel & Villas and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class HotelGuestMergeRequest(Document):
+    pass
